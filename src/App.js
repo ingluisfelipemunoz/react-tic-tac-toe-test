48a5d6e109e25208d4a58fae7fb5721c89b28537
Luis Felipe <ingluisfelipemunoz@gmail.com>
2023-10-06 23:38:16 -0400

    going back on the time/history

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState("");
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
   function handlePlay(nextSquares) {
     console.log("history", history, "nextSquares", nextSquares);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
   function handleWinner(x) {
     console.log("handling winner", x);
     setWinner(x);
   }
-  function jumpTo(nextMove) {}
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
