d8bd7204924064ffd4b3de783bb45edf18a7c748
Luis Felipe <ingluisfelipemunoz@gmail.com>
2023-10-06 23:55:34 -0400

    calculating xIsNext instead of useState

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import { useState } from "react";
 
 export default function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState("");
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
   function handlePlay(nextSquares) {
     console.log("history", history, "nextSquares", nextSquares);
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
   function handleWinner(x) {
     console.log("handling winner", x);
@@ -21,7 +20,6 @@ export default function Game() {
   function jumpTo(nextMove) {
     setWinner(null);
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
   const moves = history.map((squares, move) => {
     let description;
